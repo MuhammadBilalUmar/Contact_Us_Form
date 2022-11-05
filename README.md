2c0dc5c37894c2fd50e255f60b21036100d8661e
MuhammadBilalUmar <bilalumar001@gmail.com>
2022-11-05 14:48:59 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Contact_Us_Form
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-9-reactive-form-validation-fcerar)
---
+++ app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from './_helpers/must-match.validator';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent implements OnInit {
+    ContactUsForm: FormGroup;
+    submitted = false;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.ContactUsForm = this.formBuilder.group({
+            Name: ['', Validators.required],
+            Subject: ['', Validators.required],
+            Body: ['', Validators.required],
+            Attachement: ['', Validators.required]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.ContactUsForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.ContactUsForm.invalid) {
+            return;
+        }
+
+        // display form values on success
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.ContactUsForm.value, null, 4));
+    }
+
+    onReset() {
+        this.submitted = false;
+        this.ContactUsForm.reset();
+    }
+}
