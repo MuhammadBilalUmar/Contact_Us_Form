d7d3f4f6dcfe853ed90396bd825f7fb87098ae07
MuhammadBilalUmar <bilalumar001@gmail.com>
2022-11-09 00:54:03 +0500

    1 file modified, 1 deleted

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-// import custom validator to validate that password and confirm password fields match
-import { MustMatch } from './_helpers/must-match.validator';
-
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent implements OnInit {
     ContactUsForm: FormGroup;
